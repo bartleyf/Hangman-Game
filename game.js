c0f1934b7c68973ef5931888ff9e8fac7d8831c3
Frank Bartley <bartley.frank@gmail.com>
2017-11-09 12:12:37 -0600

    old

--- game.js
+++ game.js
@@ -1,25 +1,26 @@
-var words = ['enterprise','kirk','data','worf','torres','picard','spock','starfleet','scotty'];
+var words = ['enterprise', 'kirk', 'data', 'worf', 'torres', 'picard', 'spock', 'starfleet', 'scotty'];
 
-var alphabet = ['a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var chooseWord = words[Math.floor(Math.random() * words.length)];
 
 var arraychooseWord = chooseWord.split("");
 
-var arrayOfBlanks = arraychooseWord.map(a=>'_');
+var arrayOfBlanks = arraychooseWord.map(a => '_');
 
 document.onkeyup = function(event) {
-var userGuess = event.key;
+    var userGuess = event.key;
 
-var startValue = 0;
+    var startValue = 0;
 
-while (chooseWord.indexOf(userguess, startValue) !== -1) {
-	startValue = chooseWord.indexOf(userguess, startValue) + 1;
-}
-
-
+    while (chooseWord.indexOf(userGuess, startValue) !== -1) {
+        startValue = chooseWord.indexOf(userGuess, startValue) + 1;
+    }
 
 }
+
+
+
 
 
 
